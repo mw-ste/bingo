ff8afd3da4c1ba31f79fa1db00173c77378bc3bd
Michael Stenger <michael.stenger@outlook.de>
2022-02-27 15:09:51 +0100

    adding bingo to web app headline

--- bingo-app/src/App.tsx
+++ bingo-app/src/App.tsx
@@ -7,6 +7,10 @@ interface BingoProps {
   number: number;
 }
 
+interface BingoTextProps {
+  content: string;
+}
+
 const BingoItem = (props: BingoProps) => {
   const [active, setActive] = useState(false);
 
@@ -17,6 +21,16 @@ const BingoItem = (props: BingoProps) => {
         onClick={() => setActive(!active)}
       >
         <h1 style={{ textAlign: "center" }}>{props.number}</h1>
+      </Card>
+    </Grid>
+  );
+};
+
+const BingoTextItem = (props: BingoTextProps) => {
+  return (
+    <Grid item xs={2}>
+      <Card style={{ backgroundColor: "darkred", color: "white" }}>
+        <h1 style={{ textAlign: "center" }}>{props.content}</h1>
       </Card>
     </Grid>
   );
@@ -59,53 +73,63 @@ function App() {
   const numbers = getNumbers();
 
   return (
-    <Grid
-      container
-      spacing={2}
-      style={{
-        marginTop: 10,
-      }}
-    >
-      <Grid item xs={1} />
-      <BingoItem number={numbers[0][0]} />
-      <BingoItem number={numbers[1][0]} />
-      <BingoItem number={numbers[2][0]} />
-      <BingoItem number={numbers[3][0]} />
-      <BingoItem number={numbers[4][0]} />
-      <Grid item xs={1} />
+    <div>
+      <Grid
+        container
+        spacing={2}
+        style={{
+          marginTop: 10,
+        }}
+      >
+        <Grid item xs={1} />
+        <BingoTextItem content="B" />
+        <BingoTextItem content="I" />
+        <BingoTextItem content="N" />
+        <BingoTextItem content="G" />
+        <BingoTextItem content="O" />
+        <Grid item xs={1} />
 
-      <Grid item xs={1} />
-      <BingoItem number={numbers[0][1]} />
-      <BingoItem number={numbers[1][1]} />
-      <BingoItem number={numbers[2][1]} />
-      <BingoItem number={numbers[3][1]} />
-      <BingoItem number={numbers[4][1]} />
-      <Grid item xs={1} />
+        <Grid item xs={1} />
+        <BingoItem number={numbers[0][0]} />
+        <BingoItem number={numbers[1][0]} />
+        <BingoItem number={numbers[2][0]} />
+        <BingoItem number={numbers[3][0]} />
+        <BingoItem number={numbers[4][0]} />
+        <Grid item xs={1} />
 
-      <Grid item xs={1} />
-      <BingoItem number={numbers[0][2]} />
-      <BingoItem number={numbers[1][2]} />
-      <CenterItem />
-      <BingoItem number={numbers[3][2]} />
-      <BingoItem number={numbers[4][2]} />
-      <Grid item xs={1} />
+        <Grid item xs={1} />
+        <BingoItem number={numbers[0][1]} />
+        <BingoItem number={numbers[1][1]} />
+        <BingoItem number={numbers[2][1]} />
+        <BingoItem number={numbers[3][1]} />
+        <BingoItem number={numbers[4][1]} />
+        <Grid item xs={1} />
 
-      <Grid item xs={1} />
-      <BingoItem number={numbers[0][3]} />
-      <BingoItem number={numbers[1][3]} />
-      <BingoItem number={numbers[2][3]} />
-      <BingoItem number={numbers[3][3]} />
-      <BingoItem number={numbers[4][3]} />
-      <Grid item xs={1} />
+        <Grid item xs={1} />
+        <BingoItem number={numbers[0][2]} />
+        <BingoItem number={numbers[1][2]} />
+        <CenterItem />
+        <BingoItem number={numbers[3][2]} />
+        <BingoItem number={numbers[4][2]} />
+        <Grid item xs={1} />
 
-      <Grid item xs={1} />
-      <BingoItem number={numbers[0][4]} />
-      <BingoItem number={numbers[1][4]} />
-      <BingoItem number={numbers[2][4]} />
-      <BingoItem number={numbers[3][4]} />
-      <BingoItem number={numbers[4][4]} />
-      <Grid item xs={1} />
-    </Grid>
+        <Grid item xs={1} />
+        <BingoItem number={numbers[0][3]} />
+        <BingoItem number={numbers[1][3]} />
+        <BingoItem number={numbers[2][3]} />
+        <BingoItem number={numbers[3][3]} />
+        <BingoItem number={numbers[4][3]} />
+        <Grid item xs={1} />
+
+        <Grid item xs={1} />
+        <BingoItem number={numbers[0][4]} />
+        <BingoItem number={numbers[1][4]} />
+        <BingoItem number={numbers[2][4]} />
+        <BingoItem number={numbers[3][4]} />
+        <BingoItem number={numbers[4][4]} />
+        <Grid item xs={1} />
+      </Grid>
+    </div>
   );
 }
 
